eaf7377c7f9c9a25ddf8c47652f373d390a21c67
Johann Molano <jjdmolano@gmail.com>
2021-05-29 21:31:42 +0800

    Fixed total income and expense to show properly

--- pages/records/index.js
+++ pages/records/index.js
@@ -6,96 +6,141 @@ import RecordList from '../../components/RecordList'
 import Head from 'next/head'
 
 export default function index() {
-    const { user } = useContext(UserContext)
-    const [ name, setName ] = useState('')
-    const [ categories, setCategories ] = useState([])
-    const [ records, setRecords ] = useState([])
-    const [ incomeSum, setIncomeSum ] = useState([])
-    const [ expenseSum, setExpenseSum ] = useState([])
+	const { user } = useContext(UserContext)
+	const [name, setName] = useState('')
+	const [categories, setCategories] = useState([])
+	const [records, setRecords] = useState([])
+	const [incomeSum, setIncomeSum] = useState([])
+	const [expenseSum, setExpenseSum] = useState([])
 
-    // new array for balance per transaction
-    let balanceArr = records.map(record => record.amount),sum
-    balanceArr = balanceArr.map(recordAmount => sum = (sum || 0) + recordAmount)
+	// new array for balance per transaction
+	let balanceArr = records.map(record => record.amount),
+		sum
+	balanceArr = balanceArr.map(recordAmount => (sum = (sum || 0) + recordAmount))
 
-    // get current total balance
-    const currentBalance = balanceArr.slice(-1)
+	// get current total balance
+	const currentBalance = balanceArr.slice(-1)
 
-    // set colors for income/expense
-    const incomeStyle = {color: "#588E29"}
-    const totalIncomeStyle = {color: "#588E29"}
-    const expenseStyle = {color: "#C8040E"}
-    const totalExpenseStyle = {color: "#C8040E"}
-    const amountStyle = (currentBalance >= 0) ? incomeStyle : expenseStyle
+	// set colors for income/expense
+	const incomeStyle = { color: '#588E29' }
+	const totalIncomeStyle = { color: '#588E29' }
+	const expenseStyle = { color: '#C8040E' }
+	const totalExpenseStyle = { color: '#C8040E' }
+	const amountStyle = currentBalance >= 0 ? incomeStyle : expenseStyle
 
-    // fetch user records and categories hook
-    useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/details`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-        })
-        .then(res => res.json())
-        .then(data => {
-            setName(data.firstName)
-            setRecords(data.transactions)
-            setCategories(data.categories)
+	// fetch user records and categories hook
+	useEffect(() => {
+		fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/details`, {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('token')}`
+			}
+		})
+			.then(res => res.json())
+			.then(data => {
+				setName(data.firstName)
+				setRecords(data.transactions)
+				setCategories(data.categories)
 
-            // sum of all income
-            const incomeCalc = data.transactions
-                .filter(record => record.categoryType === 'Income')
-                .map(record => record.amount)
-                .reduce((total, amount) => total + amount, 0)
-            setIncomeSum(incomeCalc)
+				// sum of all income
+				const incomeCalc = data.transactions
+					.filter(record => record.categoryType === 'Income')
+					.map(record => record.amount)
+					.reduce((total, amount) => total + amount, 0)
+				setIncomeSum(incomeCalc)
 
-            // sum of all expenses
-            const expenseCalc = data.transactions
-                .filter(record => record.categoryType === 'Expense')
-                .map(record => record.amount)
-                .reduce((total, amount) => total + amount, 0)
-            setExpenseSum(expenseCalc)
-        })
-    },[])
+				// sum of all expenses
+				const expenseCalc = data.transactions
+					.filter(record => record.categoryType === 'Expense')
+					.map(record => record.amount)
+					.reduce((total, amount) => total + amount, 0)
+				setExpenseSum(expenseCalc)
+			})
+	}, [])
 
-    return (
-        <>
-        <Head><title>Records</title></Head>
-        {
-        (user.id === null)
-        ?   <Container className="error-container" fluid>
-                <a href="/">
-                    <Image className="error-img" src="/errorlog.svg" fluid />
-                </a>
-            </Container>
-        :   <>
-            {categories.length === 0 && records.length === 0
-            ?   <Container className="error-container" fluid>
-                    <a href="/categories">
-                        <Image className="error-img" src="/errorrecord.svg" fluid />
-                    </a>
-                </Container>
-            :   <>
-                <Jumbotron className="recordJumbotron">
-                    <Row>
-                        <Image className="recordpage-img" src="/jumbotron.png" fluid />
-                        <Col className="recordInfo">
-                            <h1>{name}'s Account</h1>
-                            <h4>Your Current Balance is: PHP <span style={amountStyle}>{currentBalance}</span></h4>
-                            <h5>Total Income: PHP <span style={totalIncomeStyle}>{incomeSum}</span></h5>
-                            <h5>Total Expenses: PHP <span style={totalExpenseStyle}>{Math.abs(expenseSum)}</span></h5>
-                        </Col>
-                    </Row>
-                </Jumbotron>
-                <AddRecord categories={categories} setRecords={setRecords} />
-                {(records.length === 0)
-                ?   <Container className="error-container" fluid>
-                        <Image className="error-img" src="/errorempty.svg" fluid></Image>
-                    </Container>
-                :   <RecordList records={records} categories={categories} setRecords={setRecords} incomeStyle={incomeStyle} expenseStyle={expenseStyle} />
-                }
-                </>
-            }
-            </>
-        }
-        </>
-    )
+	// Update total income and expenses
+	useEffect(() => {
+		const incomeCalc = records
+			.filter(record => record.categoryType === 'Income')
+			.map(record => record.amount)
+			.reduce((total, amount) => total + amount, 0)
+		setIncomeSum(incomeCalc)
+
+		const expenseCalc = records
+			.filter(record => record.categoryType === 'Expense')
+			.map(record => record.amount)
+			.reduce((total, amount) => total + amount, 0)
+		setExpenseSum(expenseCalc)
+	}, [records])
+
+	return (
+		<>
+			<Head>
+				<title>Records</title>
+			</Head>
+			{user.id === null ? (
+				<Container className='error-container' fluid>
+					<a href='/'>
+						<Image className='error-img' src='/errorlog.svg' fluid />
+					</a>
+				</Container>
+			) : (
+				<>
+					{categories.length === 0 && records.length === 0 ? (
+						<Container className='error-container' fluid>
+							<a href='/categories'>
+								<Image className='error-img' src='/errorrecord.svg' fluid />
+							</a>
+						</Container>
+					) : (
+						<>
+							<Jumbotron className='recordJumbotron'>
+								<Row>
+									<Image
+										className='recordpage-img'
+										src='/jumbotron.png'
+										fluid
+									/>
+									<Col className='recordInfo'>
+										<h1>{name}'s Account</h1>
+										<h4>
+											Your Current Balance is: PHP{' '}
+											<span style={amountStyle}>{currentBalance}</span>
+										</h4>
+										<h5>
+											Total Income: PHP{' '}
+											<span style={totalIncomeStyle}>{incomeSum}</span>
+										</h5>
+										<h5>
+											Total Expenses: PHP{' '}
+											<span style={totalExpenseStyle}>
+												{Math.abs(expenseSum)}
+											</span>
+										</h5>
+									</Col>
+								</Row>
+							</Jumbotron>
+							<AddRecord categories={categories} setRecords={setRecords} />
+							{records.length === 0 ? (
+								<Container className='error-container' fluid>
+									<Image
+										className='error-img'
+										src='/errorempty.svg'
+										fluid
+									></Image>
+								</Container>
+							) : (
+								<RecordList
+									records={records}
+									categories={categories}
+									setRecords={setRecords}
+									incomeStyle={incomeStyle}
+									expenseStyle={expenseStyle}
+								/>
+							)}
+						</>
+					)}
+				</>
+			)}
+		</>
+	)
 }
--- pages/register.js
+++ pages/register.js
@@ -2,88 +2,189 @@ import { useState, useEffect } from 'react'
 import Router from 'next/router'
 import { Container, Form, Image, Button } from 'react-bootstrap'
 import Swal from 'sweetalert2'
+import validator from 'validator'
 
 export default function register() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password1, setPassword1] = useState('')
-    const [password2, setPassword2] = useState('')
-    const [isActive, setIsActive] = useState(false)
+	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password1, setPassword1] = useState('')
+	const [password2, setPassword2] = useState('')
+	const [isActive, setIsActive] = useState(false)
+	const [emailText, setEmailText] = useState(
+		'Please input valid email address.'
+	)
+	const [passwordText, setPasswordText] = useState(
+		'Password must be at least 8 characters and not contain your first name or last name.'
+	)
+	const [password2Text, setPassword2Text] = useState('')
 
-    // verify password hook
+	// check if password contains name and is at least 8 characters
+	const passValid =
+		!(validator.contains(password1, firstName, {
+			ignoreCase: true
+		}) ||
+			validator.contains(password1, lastName, {
+				ignoreCase: true
+			})) &&
+		(firstName.length && lastName.length) !== 0
+	// check password length
+	const passLengthShort = password1.length < 8
+	// check password match
+	const passMatch = password2 === password1 ? true : false
+    // check if email is valid
+    const emailValid = validator.isEmail(email)
+	// check if all inputs are filled
+	const hasInput =
+		password1.length &&
+		password2.length &&
+		firstName.length &&
+		lastName.length &&
+		email.length !== 0
+			? true
+			: false
+
+	// password validation hook
+	useEffect(() => {
+		if (!passValid) {
+			setPasswordText('Password contains your first or last name.')
+		} else if (passLengthShort) {
+			setPasswordText(
+				'Password must be at least 8 characters and not contain your first name or last name.'
+			)
+		} else if (passValid && !passLengthShort) {
+			setPasswordText('Password is valid.')
+		}
+	}, [password1, password2, firstName, lastName])
+
+    // email validation hook
     useEffect(() => {
-        if((password1 !== '' && password2 !== '') && (password2 === password1)) {
-            setIsActive(true)
+        if (emailValid && email.length > 1) {
+            setEmailText('Email address is valid.')
         } else {
+            setEmailText('Please input valid email address.')
             setIsActive(false)
         }
-    }, [password1, password2])
+    },[email])
 
-    function registerUser(e) {
-        e.preventDefault()
+	// verify password hook & update register button on input change
+	useEffect(() => {
+		if (hasInput && passValid && !passLengthShort && emailValid) {
+			if (passMatch) {
+				setPassword2Text('')
+				setIsActive(true)
+			} else {
+				setPassword2Text('Passwords do not match.')
+				setIsActive(false)
+			}
+		} else if (!hasInput) {
+			setPassword2Text('')
+			setIsActive(false)
+		}
+	}, [password1, password2, firstName, lastName, email])
 
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password1
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            (data === true)
-            ?   Swal.fire({
-                text: 'Registration success!',
-                icon: 'success',
-                timer: 800,
-                timerProgressBar: true,
-                showConfirmButton: false
-                })
-                .then((result) => {
-                    result.dismiss === Swal.DismissReason.timer
-                    ? Router.push('/')
-                    : Router.push('/')
-                })
-            : Router.push('/error')
-        })
-    }
+	function registerUser(e) {
+		e.preventDefault()
 
-    return (
-        <Container className="register-main">
-            <div className="login-img">
-                <Image src="/landing.svg" fluid/>
-            </div>
-            <Form onSubmit={(e) => registerUser(e)} class="register-form">
-                <Form.Group>
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" value={lastName} onChange={e => setLastName(e.target.value)} required></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} required></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" value={password1} onChange={e => setPassword1(e.target.value)} required></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Verify Password</Form.Label>
-                    <Form.Control type="password" value={password2} onChange={e => setPassword2(e.target.value)} required></Form.Control>
-                </Form.Group>
-                {isActive === true
-                ? <Button className="register-button-main" type="submit" block>Register</Button>
-                : <Button id="register-button-main-disabled" type="submit" disabled block>Register</Button>}
-            </Form>
-        </Container>
-    )
+		fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				firstName: firstName,
+				lastName: lastName,
+				email: email,
+				password: password1
+			})
+		})
+			.then(res => res.json())
+			.then(data => {
+				data === true
+					? Swal.fire({
+							text: 'Registration success!',
+							icon: 'success',
+							timer: 800,
+							timerProgressBar: true,
+							showConfirmButton: false
+					  }).then(result => {
+							result.dismiss === Swal.DismissReason.timer
+								? Router.push('/')
+								: Router.push('/')
+					  })
+					: Router.push('/error')
+			})
+	}
+
+	return (
+		<Container className='register-main'>
+			<div className='login-img'>
+				<Image src='/landing.svg' fluid />
+			</div>
+			<Form onSubmit={e => registerUser(e)} class='register-form'>
+				<Form.Group>
+					<Form.Label>First Name</Form.Label>
+					<Form.Control
+						type='text'
+						value={firstName}
+						onChange={e => setFirstName(e.target.value)}
+						required
+					></Form.Control>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Last Name</Form.Label>
+					<Form.Control
+						type='text'
+						value={lastName}
+						onChange={e => setLastName(e.target.value)}
+						required
+					></Form.Control>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Email</Form.Label>
+					<Form.Control
+						type='email'
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+						required
+					></Form.Control>
+					<Form.Text className='text-muted'>{emailText}</Form.Text>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type='password'
+						value={password1}
+						onChange={e => setPassword1(e.target.value)}
+						required
+					></Form.Control>
+					<Form.Text className='text-muted'>{passwordText}</Form.Text>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Verify Password</Form.Label>
+					<Form.Control
+						type='password'
+						value={password2}
+						onChange={e => setPassword2(e.target.value)}
+						required
+					></Form.Control>
+					<Form.Text className='text-muted'>{password2Text}</Form.Text>
+				</Form.Group>
+				{isActive === true ? (
+					<Button className='register-button-main' type='submit' block>
+						Register
+					</Button>
+				) : (
+					<Button
+						id='register-button-main-disabled'
+						type='submit'
+						disabled
+						block
+					>
+						Register
+					</Button>
+				)}
+			</Form>
+		</Container>
+	)
 }
